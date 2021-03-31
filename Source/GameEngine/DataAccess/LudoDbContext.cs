f60990e8fccd1c9d9ed6037ffd5fb4c77db08e83
anton thunman jonsson <jonssonanton@hotmail.com>
2021-03-31 10:30:06 +0200

    added projects
    Added projects, installed EF. Made models.

---
+++ Source/GameEngine/DataAccess/LudoDbContext.cs
@@ -0,0 +1,17 @@
+﻿using GameEngine.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameEngine.DataAccess
+{
+    class LudoDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Piece> Pieces { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<GamePlayers> GamePlayers { get; set; }
+        public DbSet<GamePosition> GamePositions { get; set; }
+    }
+}
