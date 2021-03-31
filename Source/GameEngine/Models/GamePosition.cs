4d3e458f3a4b9be499a741266d118db5b1392ed1
andreasbrunnstrom <best_i25@hotmail.com>
2021-03-31 13:35:13 +0200

    Update GamePosition.cs
    Added [Required]

--- Source/GameEngine/Models/GamePosition.cs
+++ Source/GameEngine/Models/GamePosition.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace GameEngine.Models
@@ -7,9 +8,17 @@ namespace GameEngine.Models
     class GamePosition
     {
         public int GamePositionId { get; set; }
+
+        [Required]
         public Game Game { get; set; }
+
+        [Required]
         public User User { get; set; }
+
+        [Required]
         public string Color { get; set; }
+
+        [Required]
         public double Position { get; set; }
     }
 }
