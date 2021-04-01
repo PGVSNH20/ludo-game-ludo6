f376e60bba95bd2c5869ee3aa042733e5aa1158a
anton thunman jonsson <jonssonanton@hotmail.com>
2021-04-01 14:34:23 +0200

    Moved Console code
    Edited code to move use of console into Console project

--- Source/GameEngine/LudoEngine.cs
+++ Source/GameEngine/LudoEngine.cs
@@ -9,114 +9,45 @@ namespace GameEngine
 {
     public class LudoEngine
     {
-        private LudoDbContext Context;
+        private LudoDbContext context;
         public List<Player> Players { get; set; }
         private Random random;
         private Player winner = null;
 
         public LudoEngine()
         {
-            Context = new LudoDbContext();
+            context = new LudoDbContext();
             random = new Random();
             Players = new List<Player>();
         }
 
-        public void Create()
+        public void AddPlayer(string color, string name)
         {
-            Console.Write("Enter number of players: ");
-            int.TryParse(Console.ReadLine(), out int numOfPlayers);
-
-            AddPlayers(numOfPlayers);
-        }
-
-        public void AddPlayers(int numOfPlayers)
-        {
-            if (numOfPlayers > 1 && numOfPlayers <= 4)
-            {
-
-                string[] colors = { "Red", "Blue", "Green", "Yellow" };
-                for (int i = 0; i < numOfPlayers; i++)
-                {
-                    Console.Write($"Enter username for {colors[i]}: ");
-                    var name = Console.ReadLine();
-
-                    Player player = new Player() { Name = name };
-                    player.Pieces = new List<Piece>() {
-                        new Piece() { Color = colors[i] },
-                        new Piece() { Color = colors[i] },
-                        new Piece() { Color = colors[i] },
-                        new Piece() { Color = colors[i] }
-                    };
-                    Players.Add(player);
-                }
-            }
-            else
-            {
-                Console.Clear();
-                Console.WriteLine("Choose between 2 and 4.");
-            }
-        }
-
-        // Let's the user choose between the given list of pieces and returns that object
-        private Piece ChoosePiece(List<Piece> pieces)
-        {
-            Console.WriteLine("Which piece do you want to move?");
-            for (int i = 0; i < pieces.Count; i++)
-            {
-                Console.WriteLine($"{i}: Piece at position {pieces[i].Position}");
-            }
-            int choice = int.Parse(Console.ReadLine());
-            return pieces[choice];
+            Player player = new Player() { Name = name };
+            player.Pieces = new List<Piece>() {
+                new Piece() { Color = color },
+                new Piece() { Color = color },
+                new Piece() { Color = color },
+                new Piece() { Color = color }
+            };
+            Players.Add(player);
         }
 
         // Uses the parameters to move a piece a certain number of steps
-        private void MovePiece(Piece piece, int moves)
+        public void MovePiece(Piece piece, int moves)
         {
             piece.Position = piece.Position == 0 ? 1 : piece.Position += moves;
             Console.WriteLine($"Moved piece to position {piece.Position}!");
         }
 
-        public void Run()
+        public List<Piece> GetPiecesInNest(Player player)
         {
-            while(winner == null)
-            {
-                foreach (var player in Players)
-                {
-                    Console.WriteLine($"{player.Name} it's your turn. Enter 'r' to roll the dice.");
-                    var input = Console.ReadLine();
-                    if (input == "r")
-                    {
-                        List<Piece> PiecesInNest = player.Pieces.Where(p => p.Position == 0).ToList();
-                        List<Piece> PiecesInPlay = player.Pieces.Where(p => p.Position != 0).ToList();
-                        int moves = ThrowDice();
-                        Console.WriteLine($"You got a {moves}!");
+            return player.Pieces.Where(p => p.Position == 0).ToList();
+        }
 
-                        if (moves == 6)
-                        {
-                            // If all pieces are in the nest, automatically move one out
-                            if (PiecesInNest.Count == 4)
-                                MovePiece(player.Pieces[0], 1);
-                            // If there are pieces both in the nest and in play, let the user choose one of all pieces and move it.
-                            else
-                                MovePiece(ChoosePiece(player.Pieces), moves);
-
-                            continue;
-                        }
-
-                        // The code below only executes if 'moves' is not equal to 6
-
-                        // If there's only one piece in play, automatically move the piece
-                        if (PiecesInPlay.Count == 1)
-                        {
-                            MovePiece(PiecesInPlay[0], moves);
-                            continue;
-                        }
-                        // If there are multiple pieces in play, let the user choose between those pieces and move it.
-                        if (PiecesInPlay.Count > 1)
-                            MovePiece(ChoosePiece(PiecesInPlay), moves);
-                    }
-                }
-            }
+        public List<Piece> GetPiecesInPlay(Player player)
+        {
+            return player.Pieces.Where(p => p.Position != 0).ToList();
         }
 
         public void Load()
@@ -144,7 +75,7 @@ namespace GameEngine
         public User GetUserByName(string name)
         {
             try {
-                return Context.Users.Where(u => u.Name == name).Single();
+                return context.Users.Where(u => u.Name == name).Single();
             } catch { return null; }
         }
     }
--- Source/LudoConsole/Program.cs
+++ Source/LudoConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using GameEngine;
 using System;
+using System.Collections.Generic;
 
 namespace LudoConsole
 {
@@ -9,20 +10,26 @@ namespace LudoConsole
         {
             int choice;
             LudoEngine game = new LudoEngine();
+            Player winner = null;
+
             do
             {
-                Console.WriteLine("What do you want do?");
-                Console.WriteLine("1: Start new game");
-                Console.WriteLine("2: Load game");
-                Console.WriteLine("3: Show user statistics");
-                Console.WriteLine("9: Exit");
-                int.TryParse(Console.ReadLine(), out choice);
+                choice = ChooseFromMainMenu();
 
                 switch (choice)
                 {
                     case 1:
-                        game.Create();
-                        game.Run();
+                        int numberOfPlayers = AskForNumberOfPlayers();
+                        AddPlayers(numberOfPlayers, game);
+                        while(winner == null)
+                        {
+                            foreach (var player in game.Players)
+                            {
+                                int moves = LetPlayerRollDice(player, game);
+                                HandleMoves(moves, player, game);
+
+                            }
+                        }
                         break;
                     case 2: game.Load();
                         break;
@@ -35,5 +42,100 @@ namespace LudoConsole
             while (choice != 9);
 
         }
+
+        private static void HandleMoves(int moves, Player player, LudoEngine game)
+        {
+            List<Piece> PiecesInNest = game.GetPiecesInNest(player);
+            List<Piece> PiecesInPlay = game.GetPiecesInPlay(player);
+
+            if (moves == 6)
+            {
+                // If all piece are in the nest, automatically move one out
+                if (PiecesInNest.Count == 4)
+                {
+                    game.MovePiece(player.Pieces[0], 1);
+                }
+                // If there are pieces both in the nest and in play, let the user choose one of all pieces and move it.
+                else
+                    game.MovePiece(LetPlayerChoosePiece(player.Pieces, game), moves);
+            }
+            else
+            {
+                // If there's only one piece in play, automatically move the piece
+                if (PiecesInPlay.Count == 1)
+                {
+                    game.MovePiece(PiecesInPlay[0], moves);
+                }
+                // If there are multiple pieces in play, let the user choose between those pieces and move it.
+                if (PiecesInPlay.Count > 1)
+                {
+                    game.MovePiece(LetPlayerChoosePiece(PiecesInPlay, game), moves);
+                }
+            }
+        }
+
+        private static Piece LetPlayerChoosePiece(List<Piece> pieces, LudoEngine game)
+        {
+            Console.WriteLine("Which piece do you want to move?");
+            for (int i = 0; i < pieces.Count; i++)
+            {
+                Console.WriteLine($"{i}: Piece at position {pieces[i].Position}");
+            }
+            int choice = int.Parse(Console.ReadLine());
+            return pieces[choice];
+        }
+
+        private static int LetPlayerRollDice(Player player, LudoEngine game)
+        {
+            Console.WriteLine($"{player.Name} it's your turn. Enter 'r' to roll the dice.");
+            var input = Console.ReadLine();
+
+            if (input == "r")
+            {
+                int moves = game.ThrowDice();
+                Console.WriteLine($"You got a {moves}!");
+                return moves;
+            }
+            return 0;
+        }
+
+        private static int ChooseFromMainMenu()
+        {
+            Console.WriteLine("What do you want do?");
+            Console.WriteLine("1: Start new game");
+            Console.WriteLine("2: Load game");
+            Console.WriteLine("3: Show user statistics");
+            Console.WriteLine("9: Exit");
+            int.TryParse(Console.ReadLine(), out int choice);
+            return choice;
+        }
+
+        private static int AskForNumberOfPlayers()
+        {
+            int numOfPlayers = 0;
+            do
+            {
+                Console.Write("Enter number of players (between 2 and 4): ");
+                int.TryParse(Console.ReadLine(), out numOfPlayers);
+
+            } while (numOfPlayers > 4 || numOfPlayers < 2);
+
+            return numOfPlayers;
+        }
+
+        private static void AddPlayers(int numOfPlayers, LudoEngine game)
+        { 
+            for(int i = 0; i < numOfPlayers; i++)
+            {
+                string[] colors = { "Red", "Blue", "Green", "Yellow" };
+                Console.Write($"Enter username for {colors[i]}: ");
+                var name = Console.ReadLine();
+
+                game.AddPlayer(colors[i], name);
+
+            }
+            
+        }
     }
+
 }
